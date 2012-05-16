c2b82e01b6d920c6efbcb0499f839b0e459097fb
walh <walh@zhaw.ch>
2012-05-16 06:25:54 +0000

    sw_stack_prp1/release/release.sh: build script modified sw_stack_prp1/release/changelog.txt: version 1.1 with modified seq number
    git-svn-id: https://parma.zhaw.ch/svn/prp/trunk/sw_stack_prp1@442 15592ae9-fe23-0410-b32f-b447283933e9

--- release/release.sh
+++ release/release.sh
@@ -1,46 +1,74 @@
 #!/bin/bash
 # @author   walh@zhaw.ch
-# @date     11-04-2012
+# @date     16-05-2012
 # @brief    release build script for sw_stack PRP-1
 ### ----------------------------------------------------------------------------
 set -e
 
-# log info
-echo "#################################################"
-echo "### build SW_Stack_PRP-1                      ###"
-echo "#################################################"
-echo "-> doc"
-echo "-> prp"
-echo "-> prp_pcap_tap_userspace"
+### log info -------------------------------------------------------------------
+echo "#########################################################################"
+echo "### Build SW_Stack_PRP-1                                              ###"
+echo "#########################################################################"
 
-# remove old builds, create new build ------------------------------------------
+### remove old builds, create new build ----------------------------------------
 rm -rf build
 mkdir build
 
-# get release number -----------------------------------------------------------
-if head -n 1 changelog.txt | grep '^Release '; then
-    version=$(head -n 1 changelog.txt | sed -e 's/^Release //' | tr -d '[:space:]')
+### get release number ---------------------------------------------------------
+echo "### Build new release"
+if head -n 1 changelog.txt | grep '^Release ' ; then
+    VERSION=$(head -n 1 changelog.txt | sed -e 's/^Release //' | tr -d '[:space:]')
 fi
+mkdir build/sw_stack_prp1_v_${VERSION}
+echo
 
-
-# copy documentation
-mkdir build/doc
+### copy documentation and changelog--------------------------------------------
+mkdir build/sw_stack_prp1_v_${VERSION}/doc
+echo "### Copy documentation"
 if [ -f ../doc/PRP-1_stack.pdf ]; then
-    cp ../doc/PRP-1_stack.pdf build/doc/
+    cp ../doc/PRP-1_stack.pdf build/sw_stack_prp1_v_${VERSION}/doc/
+    echo "PRP-1_stack.pdf"
 else
-    echo "Docu does not exist"
+    echo "Docu PRP-1_stack.pdf does not exist"
     exit 1
 fi
+if [ -f changelog.txt ] ; then
+    cp changelog.txt build/sw_stack_prp1_v_${VERSION}/
+    echo "changelog.txt"
+fi
+echo
 
-# svn exports ------------------------------------------------------------------
-svn export ../prp/ build/prp
-svn export ../prp_pcap_tap_userspace build/prp_pcap_tap_userspace
+### SVN version ----------------------------------------------------------------
+if [ -d .svn ] ; then
+    echo "### Get SVN version"
+    SVN_REVISION=$(svnversion ..)
+    SVN_URL=$(svn info ..|grep ^URL $hdl|sed -e 's/^URL: //')
+    echo "SVN version: $SVN_REVISION"
+    echo "Releases in changelog.txt:"
+    grep -i ^release changelog.txt | head -n 3
+fi
+echo
 
-# create zip file --------------------------------------------------------------
+### SVN exports ----------------------------------------------------------------
+echo "### Export SVN repository"
+echo "-> prp"
+svn export --force ../prp/ build/sw_stack_prp1_v_${VERSION}/prp
+echo "-> prp_pcap_tap_userspace"
+svn export --force ../prp_pcap_tap_userspace build/sw_stack_prp1_v_${VERSION}/prp_pcap_tap_userspace
+echo
+
+### SVN logfile ----------------------------------------------------------------
+echo "Release version : $VERSION" > build/sw_stack_prp1_v_${VERSION}/release_version.txt
+echo "SVN revision    : $SVN_REVISION ($SVN_URL)" >> build/sw_stack_prp1_v_${VERSION}/release_version.txt
+echo "Packaging date  : $(LANG=C date) " >> build/sw_stack_prp1_v_${VERSION}/release_version.txt
+echo >> build/sw_stack_prp1_v_${VERSION}/release_version.txt
+
+### Create zip file ------------------------------------------------------------
+echo "### Build zip file"
 cd build
-zip --quiet -r sw_stack_prp-1.zip  *
+zip --quiet -r ../sw_stack_prp1_v_${VERSION}.zip  *
+echo "done..."
+echo
 
-echo "-> build/sw_stack_prp-1.zip"
-echo "### done..."
 
 exit 0
